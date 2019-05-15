05d055567fe7092aeaccd2d8c862d9959b491e29
Dave Martin <djtfmartin@gmail.com>
2019-05-15 21:06:28 +0100

    1.8.5
    added the automatic rotation based on exif information. this will fix
    
    https://github.com/AtlasOfLivingAustralia/image-service/issues/4

--- src/main/java/au/org/ala/images/util/ImageReaderUtils.java
+++ src/main/java/au/org/ala/images/util/ImageReaderUtils.java
@@ -1,10 +1,23 @@
 package au.org.ala.images.util;
 
+import au.org.ala.images.metadata.ImageMetadataExtractor;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.exif.ExifIFD0Directory;
+import com.drew.metadata.jpeg.JpegDirectory;
+import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import javax.imageio.stream.ImageInputStream;
-import java.io.IOException;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.DataBufferByte;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 
 public class ImageReaderUtils {
@@ -47,9 +60,7 @@ public class ImageReaderUtils {
 
             try {
                 if (result != null) {
-                    fbis = new FastByteArrayInputStream(imageBytes);
-                    iis = ImageIO.createImageInputStream(fbis);
-                    result.setInput(iis);
+                    result.setInput(rotate(imageBytes));
                     return result;
                 }
             } catch (IOException ioex) {
@@ -62,4 +73,83 @@ public class ImageReaderUtils {
         return null;
     }
 
+    public static ImageInputStream  rotate(byte[] imageBytes) throws IOException {
+
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new ByteArrayInputStream(imageBytes));
+            Metadata metadata = ImageMetadataReader.readMetadata(bis, imageBytes.length);
+            Collection<ExifIFD0Directory> exifIFD0 = metadata.getDirectoriesOfType(ExifIFD0Directory.class);
+            JpegDirectory jpegDirectory = metadata.getFirstDirectoryOfType(JpegDirectory.class);
+
+            if(jpegDirectory == null || exifIFD0.isEmpty()){
+                FastByteArrayInputStream fbis = new FastByteArrayInputStream(imageBytes);
+                return ImageIO.createImageInputStream(fbis);
+            }
+
+            int orientation = exifIFD0.iterator().next().getInt(ExifIFD0Directory.TAG_ORIENTATION);
+
+            int width = jpegDirectory.getImageWidth();
+            int height = jpegDirectory.getImageHeight();
+
+            AffineTransform affineTransform = new AffineTransform();
+
+            switch (orientation) {
+                case 1:
+                    break;
+                case 2: // Flip X
+                    affineTransform.scale(-1.0, 1.0);
+                    affineTransform.translate(-width, 0);
+                    break;
+                case 3: // PI rotation
+                    affineTransform.translate(width, height);
+                    affineTransform.rotate(Math.PI);
+                    break;
+                case 4: // Flip Y
+                    affineTransform.scale(1.0, -1.0);
+                    affineTransform.translate(0, -height);
+                    break;
+                case 5: // - PI/2 and Flip X
+                    affineTransform.rotate(-Math.PI / 2);
+                    affineTransform.scale(-1.0, 1.0);
+                    break;
+                case 6: // -PI/2 and -width
+                    affineTransform.translate(height, 0);
+                    affineTransform.rotate(Math.PI / 2);
+                    break;
+                case 7: // PI/2 and Flip
+                    affineTransform.scale(-1.0, 1.0);
+                    affineTransform.translate(-height, 0);
+                    affineTransform.translate(0, width);
+                    affineTransform.rotate(3 * Math.PI / 2);
+                    break;
+                case 8: // PI / 2
+                    affineTransform.translate(0, width);
+                    affineTransform.rotate(3 * Math.PI / 2);
+                    break;
+                default:
+                    break;
+            }
+
+            BufferedInputStream bis2 = new BufferedInputStream(new ByteArrayInputStream(imageBytes));
+            BufferedImage originalImage = ImageIO.read(bis2);
+
+
+            AffineTransformOp affineTransformOp = new AffineTransformOp(affineTransform, AffineTransformOp.TYPE_BILINEAR);
+            BufferedImage destinationImage = new BufferedImage(originalImage.getHeight(), originalImage.getWidth(), originalImage.getType());
+            destinationImage = affineTransformOp.filter(originalImage, destinationImage);
+
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            ImageIO.write(destinationImage, "jpg", baos);
+            baos.flush();
+            byte[] imageInByte = baos.toByteArray();
+            baos.close();
+
+            FastByteArrayInputStream fbis = new FastByteArrayInputStream(imageInByte);
+            return ImageIO.createImageInputStream(fbis);
+        } catch (Exception e){
+            FastByteArrayInputStream fbis = new FastByteArrayInputStream(imageBytes);
+            return ImageIO.createImageInputStream(fbis);
+        }
+    }
 }
